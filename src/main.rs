6bdf470e4b784a26baafa2a23e75b5990e2b4e2e
tkhshmsy <tkhshmsy@gmail.com>
2022-11-13 15:00:12 +0900

    Game Logic In Rust
        - https://slint-ui.com/releases/0.3.1/docs/tutorial/rust/game_logic_in_rust.html

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,36 @@ fn main() {
     let mut rng = rand::thread_rng();
     tiles.shuffle(&mut rng);
 
+    // Assign the shuffled Vec to the model property
     let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
-    main_window.set_memory_tiles(tiles_model.into());
+    main_window.set_memory_tiles(tiles_model.clone().into());
+
+    let main_window_weak = main_window.as_weak();
+    main_window.on_check_if_pair_solved(move || {
+        let mut flipped_tiles = 
+            tiles_model.iter().enumerate().filter( | (_, tile) | tile.image_visible && !tile.solved);
+        
+        if let (Some((t1_idx, mut t1)), Some((t2_idx, mut t2))) = (flipped_tiles.next(), flipped_tiles.next()) {
+            let is_pair_solved = t1 == t2;
+            if is_pair_solved {
+                t1.solved = true;
+                tiles_model.set_row_data(t1_idx, t1);
+                t2.solved = true;
+                tiles_model.set_row_data(t2_idx, t2);
+            } else {
+                let main_window = main_window_weak.unwrap();
+                main_window.set_disable_tiles(true);
+                let tiles_model = tiles_model.clone();
+                slint::Timer::single_shot(std::time::Duration::from_secs(1), move || {
+                    main_window.set_disable_tiles(false);
+                    t1.image_visible = false;
+                    tiles_model.set_row_data(t1_idx, t1);
+                    t2.image_visible = false;
+                    tiles_model.set_row_data(t2_idx, t2);
+                });
+            }
+        }
+    });
 
     main_window.run();
 }
@@ -73,6 +101,9 @@ slint::slint! {
         width: 326px;
         height: 326px;
 
+        callback check_if_pair_solved();
+        property <bool> disable_tiles;
+
         property <[TileData]> memory_tiles : [
             { image: @image-url("icons/at.png") },
             { image: @image-url("icons/balance-scale.png") },
@@ -93,7 +124,10 @@ slint::slint! {
             // propagate the solved status from the model to the tile
             solved: tile.solved;
             clicked => {
-                self.open_curtain = !self.open_curtain;
+                if (!root.disable_tiles) {
+                    tile.image_visible = !tile.image_visible;
+                    root.check_if_pair_solved();
+                }
             }
         }
     }
