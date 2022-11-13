28f1d1712bc097dd85dddde8a297c840d562de1a
tkhshmsy <tkhshmsy@gmail.com>
2022-11-13 14:34:16 +0900

    Creating The Tiles From Rust
        - https://slint-ui.com/releases/0.3.1/docs/tutorial/rust/creating_the_tiles_from_rust.html

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 fn main() {
-    MainWindow::new().run();
+    use slint::Model;
+
+    let main_window = MainWindow::new();
+    
+    // Fetch the tiles from the model
+    let mut tiles: Vec<TileData> = main_window.get_memory_tiles().iter().collect();
+    // Duplicate them to ensure that we have pairs
+    tiles.extend(tiles.clone());
+
+    // Randomly mix the tiles
+    use rand::seq::SliceRandom;
+    let mut rng = rand::thread_rng();
+    tiles.shuffle(&mut rng);
+
+    let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
+    main_window.set_memory_tiles(tiles_model.into());
+
+    main_window.run();
 }
 
 slint::slint! {
